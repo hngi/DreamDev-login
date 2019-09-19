81808625e88b87ed24c11b5290ae3d2728cf9dee
Daniel Ufeli <danielufeli@yahoo.com>
2019-09-19 15:46:52 +0100

    change cleardb path

--- includes/server.php
+++ includes/server.php
@@ -1,20 +1,11 @@
 <?php
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$server = $url["us-cdbr-iron-east-02.cleardb.net"];
-$username = $url["b916489ac9ce85"];
-$password = $url["c55e02df"];
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
 $dbname = substr($url["path"], 1);
 
 $db = new mysqli($server, $username, $password, $dbname);
 
-
-$sql = "SELECT * FROM `users` WHERE `email`='".$db->real_escape_string(email)."'";
-
-if($result = $db->query($sql)) {
-    foreach($result as $row) {
-        // ...
-    }
-} else {
-    throw new Exception($db->error);
-}
+?>
