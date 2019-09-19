c2e95d2f5f77a0d8e8d62a88156ae3437d370a38
Daniel Ufeli <danielufeli@yahoo.com>
2019-09-19 15:24:20 +0100

    update cleardb

--- includes/server.php
+++ includes/server.php
@@ -1,30 +1,10 @@
-<?php 
-	
-	// connect to database
-	// $db = mysqli_connect('localhost', 'root', '', 'dreamdev');
-	
-	$active_group = 'default';
-	$query_builder = TRUE;
-	
-	$db['default'] = array(
-		'dsn'    => '',
-		'hostname' => 'us-cdbr-iron-east-02.cleardb.net',
-		'username' => 'b916489ac9ce85',
-		'password' => 'c55e02df',
-		'database' => 'heroku_6a8f30a1d0f6b34',
-		'dbdriver' => 'mysqli',
-		'dbprefix' => '',
-		'pconnect' => FALSE,
-		'db_debug' => (ENVIRONMENT !== 'production'),
-		'cache_on' => FALSE,
-		'cachedir' => '',
-		'char_set' => 'utf8',
-		'dbcollat' => 'utf8_general_ci',
-		'swap_pre' => '',
-		'encrypt' => FALSE,
-		'compress' => FALSE,
-		'stricton' => FALSE,
-		'failover' => array(),
-		'save_queries' => TRUE
-	);
+<?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["us-cdbr-iron-east-02.cleardb.net"];
+$username = $url["b916489ac9ce85"];
+$password = $url["c55e02df"];
+$db = substr($url["heroku_6a8f30a1d0f6b34"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
 ?>
