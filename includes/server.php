3d2332717b5431eba10948b8a52a5cb385499875
Daniel Ufeli <danielufeli@yahoo.com>
2019-09-19 15:41:39 +0100

    change cleardb path

--- includes/server.php
+++ includes/server.php
@@ -7,4 +7,14 @@ $password = $url["c55e02df"];
 $dbname = substr($url["path"], 1);
 
 $db = new mysqli($server, $username, $password, $dbname);
-?>
+
+
+$sql = "SELECT * FROM `users` WHERE `email`='".$db->real_escape_string(email)."'";
+
+if($result = $db->query($sql)) {
+    foreach($result as $row) {
+        // ...
+    }
+} else {
+    throw new Exception($db->error);
+}
